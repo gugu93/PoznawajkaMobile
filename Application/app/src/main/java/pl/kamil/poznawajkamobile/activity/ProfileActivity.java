4e40ed2c632fcb3d5896c2a5ba2b58f616a372dc
Adrian <adex333@gmail.com>
2015-05-22 01:14:34 +0200

    Poprawki w layoutach cz.6 i poprawki w aktywnościach

--- Application/app/src/main/java/pl/kamil/poznawajkamobile/activity/ProfileActivity.java
+++ Application/app/src/main/java/pl/kamil/poznawajkamobile/activity/ProfileActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.kml.poznawajkamobile.R;
@@ -14,15 +15,36 @@ import com.example.kml.poznawajkamobile.R;
 
 public class ProfileActivity extends ActionBarActivity {
     private Button myButton;
-    private TextView myText;
-    private TextView myToDialog;
-    private TextView autoDialog;
+    private TextView profileEmail;
+    private TextView profileName;
+    private TextView profileLastName;
+    private TextView profilePreferences;
+    private TextView profileAppearance;
+    private ImageView profile_gallery;
+    private ImageView profile_friends;
+    private TextView zmienzdjecie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
         myButton = (Button)findViewById(R.id.buttonMeetNewPpl);
+        profile_gallery = (ImageView)findViewById(R.id.profile_gallery);
+        profile_gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), GalleryActivity.class);
+                startActivity(intent);
+            }
+        });
+        profile_friends = (ImageView)findViewById(R.id.profile_friends);
+        profile_friends.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(getApplicationContext(), FriendsActivity.class);
+                //startActivity(intent);
+            }
+        });
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,20 +52,12 @@ public class ProfileActivity extends ActionBarActivity {
                 startActivity(intent);
             }
         });
-        myText = (TextView)findViewById(R.id.zmienzdjecie);
-        myText.setOnClickListener(new View.OnClickListener() {
+        zmienzdjecie = (TextView)findViewById(R.id.zmienzdjecie);
+        zmienzdjecie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
             Intent intent = new Intent(getApplicationContext(),GalleryActivity.class);
             startActivity(intent);
-            }
-        });
-        myToDialog = (TextView)findViewById(R.id.profil);
-        myToDialog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            Intent intent = new Intent(getApplicationContext(),AppointmentActivity.class);
-                startActivity(intent);
             }
         });
 
